dd7694135531ecfb0b9e9c0cc6ad7d098e661ba7
Marion Kerioui <marion.kerioui@mrc-bsu.cam.ac.uk>
2024-08-06 12:52:05 +0100

    Optimization lambda with MSE

--- R/single_stage.R
+++ R/single_stage.R
@@ -13,6 +13,7 @@
 #' @export
 #'
 #' @examples
+#'single_stage(A=3)
 #'
 single_stage<-function(p_0 = 0.15, p_A = 0.45, A, K = 5, n = 12, lambda = 0.004, gamma = 1, pi_critic = 0.34 ){
   data_simu <- emulate_trial(p_0 = p_0, p_A = p_A, A = A, K = K, n = n)
@@ -26,7 +27,7 @@ single_stage<-function(p_0 = 0.15, p_A = 0.45, A, K = 5, n = 12, lambda = 0.004,
 
 #' Select the optimal critical value for the final analysis by targeting a specific FWER when no basket is truly active
 #'
-#'@param p_0 the null probability of response, default is 0.15
+#' @param p_0 the null probability of response, default is 0.15
 #' @param p_A the active probability of response, default is 0.45
 #' @param K the total number of baskets
 #' @param n the sample size in each basket (give either a vector of length K or a single numeric value)
@@ -35,24 +36,25 @@ single_stage<-function(p_0 = 0.15, p_A = 0.45, A, K = 5, n = 12, lambda = 0.004,
 #' @param FWER_target the Family Wise Error Rate targeted
 #' @param pi_critic_min the minimum value considered in the grid of optimization for the critical value
 #' @param pi_critic_max the maximum value considered in the grid of optimization for the critical value
-#' @param pi_critic_step the steps length of the grid for the critical values
+#' @param pi_critic_step the steps of the grid for the critical values
 #' @param nb_replicate the number of simulated trials
 #' @param parallel.option boolean, TRUE if simulations should be ran in parallel
+#' @param nb_cores integer, number of cores for parallelization
 #'
 #' @return two numerical values; the first one pi_critic is the optimal critical values among the ones considered
 #' the second value is the FWER obtained with this given critical value
 #' @export
 #'
 #' @examples
+#'calibrate_finalDecision(pi_critic_step = 0.1, pi_critic_min=0.3, nb_replicate = 100)
 #'
-calibrate_finalDecision<-function(p_0 = 0.15, p_A = 0.45, K = 5, n = 12, lambda = 0.004, gamma = 1, FWER_target = 0.1, pi_critic_min = 0.1, pi_critic_max = 0.5, pi_critic_step = 0.05, nb_replicate = 10000, parallel.option = TRUE ){
+calibrate_finalDecision<-function(p_0 = 0.15, p_A = 0.45, K = 5, n = 12, lambda = 0.004, gamma = 1, FWER_target = 0.1, pi_critic_min = 0.1, pi_critic_max = 0.5, pi_critic_step = 0.05, nb_replicate = 10000, parallel.option = TRUE, nb_cores = 2 ){
   stop_proba(FWER_target)#checking that the target FWER is between 0 and 1
 
   pi_critic_grid <- seq(pi_critic_min, pi_critic_max, pi_critic_step)
 
   if(parallel.option){
     pbapply::pboptions(use_lb=TRUE)
-    nb_cores<-min(parallel::detectCores()-2,length(pi_critic_grid)  )
     cl<-parallel::makeCluster(nb_cores)
     FWER_grid <- pbapply::pbsapply(1:length(pi_critic_grid),cl=cl,
                                               function (j) mean(sapply(1:nb_replicate,
@@ -72,6 +74,42 @@ calibrate_finalDecision<-function(p_0 = 0.15, p_A = 0.45, K = 5, n = 12, lambda 
 }
 
 
+
+
+
+#'  Compute average MSE under a given scenario of activity
+#' for a grid of values for lambda and select the lambda that minimizes the average MSE
+#'
+#' @param p_0 the null probability of response, default is 0.15
+#' @param p_A the active probability of response, default is 0.45
+#' @param A the number of active baskets
+#' @param K the total number of baskets
+#' @param n the sample size in each basket (give either a vector of length K or a single numeric value)
+#' @param lambda_min minimum value considered for the grid of values of lambda
+#' @param lambda_max maximum value considered for the grid of values of lambda
+#' @param lambda_step the steps of the grid for of values for lambda
+#' @param gamma controls the weigths of L1 and L2 norms
+#' @param pi_critic the critical value of the final decision
+#' @param nb_replicate the number of simulated trials
+#'
+#' @return the average MSE for each value of lambda and the lambda minimizing the average MSE
+#' @export
+#'
+#' @examples
+#' computeMSE(nb_replicate = 100)
+#'
+computeMSE<-function(p_0 = 0.15, p_A = 0.45, A = 3, K = 5, n = 12, lambda_min = 0, lambda_max = 0.01, lambda_step = 0.001, gamma = 1, pi_critic = 0.35, nb_replicate = 10000){
+  lambda_grid <- seq(lambda_min, lambda_max, lambda_step)
+  MSE_grid <- pbapply::pbsapply(1:length(lambda_grid),  function(j) mean(Reduce(rbind, lapply(1:nb_replicate,
+                               function(l) single_stage(p_0 = p_0, p_A = p_A, A = A, K = K, n = n,
+                                                       lambda = lambda_grid[j], gamma = gamma,
+                                                        pi_critic = pi_critic)$sq_diff))))
+  lambda <- lambda_grid[which.min(MSE_grid)]
+  MSE<-cbind(lambda_grid, MSE_grid)
+  colnames(MSE)<- c("lambda", "MSE")
+
+  create_named_list(lambda, MSE)
+}
 
 
 
